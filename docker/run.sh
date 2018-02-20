f065ca1d0a3aa0cc8397dd17be332d40a042e9a8
little.mole <little.mole@oha7.org>
2018-02-20 23:45:36 +0100

    docker

--- docker/run.sh
+++ docker/run.sh
@@ -26,8 +26,8 @@ else
     mkdir build
     cd build
     cmake .. -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_CXX_FLAGS=$FLAGS
-    make -e
-    make -e test
-    make -e install
+    make 
+    make test
+    make install
 fi
 
