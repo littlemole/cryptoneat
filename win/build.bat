210ec7984c4a515289c5e4ae2f14414dddfd1180
littlemole <littlemole@oha7.org>
2023-10-02 19:05:08 +0200

    rely on visual studio vcpkg

--- win/build.bat
+++ win/build.bat
@@ -1,11 +1,11 @@
 cd out\build\x64-Debug
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=%~dp0\..\vcpkg\scripts\buildsystems\vcpkg.cmake ../../..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" ../../..
 cmake --build .
 
 cd ..
 
 cd x64-Release
-cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE=%~dp0\..\vcpkg\scripts\buildsystems\vcpkg.cmake ../../..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" ../../..
 cmake --build .
 
 cd ..
