2479a8d16faaa0ef8f81bb06ce23200356b7cc1c
littlemole <littlemole@oha7.org>
2023-10-02 21:15:34 +0200

    Revert "rely on visual studio vcpkg"
    This reverts commit 210ec7984c4a515289c5e4ae2f14414dddfd1180.

--- win/bootstrap.bat
+++ win/bootstrap.bat
@@ -1,3 +1,13 @@
+@echo off
+if not exist vcpkg (
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg
+cmd /C bootstrap-vcpkg.bat
+cd ..
+)
+
+
+
 
 if not exist "out" mkdir out
 cd out
@@ -9,7 +19,7 @@ cd ..
 cd ..
 
 
-set CMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
+set CMAKE_TOOLCHAIN_FILE=%~dp0vcpkg\scripts\buildsystems\vcpkg.cmake
 echo CMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE%
 
 cmd /C win\build.bat
